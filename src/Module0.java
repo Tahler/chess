9777b8fca8671e5d2d21844afd2ab63946406791
Tahler <tyler@berryac.com>
2015-08-05 14:34:11 -0600

    have some preliminary regex

--- src/Module0.java
+++ src/Module0.java
@@ -4,6 +4,19 @@
  */
 public class Module0 {
 
+    /*
+        // Piece placement: ([kqbnrp])([ld])([a-h])([1-8])
+        kld8
+        qda3
+
+        // Single-piece movement: ([a-h])([1-8])([a-h])([1-8])(\*)?
+        d1e7
+        h4e3*
+
+        // Two-Piece Movement: ([a-h])([1-8])([a-h])([1-8])([a-h])([1-8])([a-h])([1-8])
+        d1e7e4b6
+     */
+
     public static void main(String[] args) {
         
     }
